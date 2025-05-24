8e965e1ecce1668e42c0c538b9ad492aafaae212
João Victor Alves <joaov.alves018@gmail.com>
2025-05-24 13:50:04 -0300

    Setup inicial

---
+++ src/main/java/com/github/joaoalves_dev/ApiLojaPerfume/ApiLojaPerfume/ApiLojaPerfumeApplication.java
@@ -0,0 +1,13 @@
+package com.github.joaoalves_dev.ApiLojaPerfume.ApiLojaPerfume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLojaPerfumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLojaPerfumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/joaoalves_dev/ApiLojaPerfume/ApiLojaPerfume/ApiLojaPerfumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joaoalves_dev.ApiLojaPerfume.ApiLojaPerfume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLojaPerfumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
